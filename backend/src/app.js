3c00d4b07e0ac150d851b36cd1cec7fee1aa1e44
Roland Klahitar <klahitarroland@gmail.com>
2025-10-16 03:27:53 -0500

    sign up service

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { postsRoutes } from "./routes/posts.js ";
+import { userRoutes } from "./routes/users.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+
+
+
 postsRoutes(app);
+userRoutes(app);
 
 export { app };
-
-
--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -4,8 +4,8 @@ export function initDatabase() {
   mongoose.connection.on("open", () => {
     console.info("successfully connected to database:", DATABASE_URL);
   });
-  //const DATABASE_URL = "mongodb://localhost:27017/blog";
-  const DATABASE_URL = process.env.DATABASE_URL;
+  const DATABASE_URL = "mongodb://localhost:27017/blog";
+  //const DATABASE_URL = process.env.DATABASE_URL;
   const connection = mongoose.connect(DATABASE_URL);
   return connection;
 }
