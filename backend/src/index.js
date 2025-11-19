3a3433f26d853e4dae7e111f3f612188f53c6bf1
Roland Klahitar <klahitarroland@gmail.com>
2025-11-19 02:19:53 -0600

    env var

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import { initDatabase } from "./db/init.js";
 
 try {
   await initDatabase();
+
   const PORT = process.env.PORT || 8080;
-  app.listen(PORT,"0.0.0.0", ()=> {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server listening on port", PORT);
   });
 } catch (err) {
   console.error("error connecting to database:", err);
