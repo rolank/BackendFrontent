809b8d9b23207648d3e2ab6ce382db6ff2139b02
Roland Klahitar <klahitarroland@gmail.com>
2025-11-19 00:07:47 -0600

    hostname address

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import { initDatabase } from "./db/init.js";
 try {
   await initDatabase();
   const PORT = process.env.PORT || 8080;
-  app.listen(PORT,'0.0.0.0', ()=> {
+  app.listen(PORT,"0.0.0.0", ()=> {
     console.log(`Server is running on port ${PORT}`);
   });
 } catch (err) {
