0b0ea9c933e9d3dbc782d427a0e3eed6b2110556
Roland Klahitar <klahitarroland@gmail.com>
2025-11-21 00:20:43 -0600

    new DB code

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { app } from "./app.js";
-//import { initDatabase } from "./db/init.js";
+import { initDatabase } from "./db/init.js";
 
 const port = process.env.PORT || 8080;
 
