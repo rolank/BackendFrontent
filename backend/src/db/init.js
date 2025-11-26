bd3717551174c25224474c2ed7a50a014d52ade2
Roland Klahitar <klahitarroland@gmail.com>
2025-11-26 11:08:48 -0600

    change in build arg

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export function initDatabase() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL environment variable is required");
+  }
   const DATABASE_URL = process.env.DATABASE_URL;
+
   mongoose.connection.on("open", () => {
     console.info("successfully connected to database:");
   });
